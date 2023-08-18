10f10bab6c060b371a8b3798a0f0daf9e60d4979
Ariel Diego <arieldiego73@gmail.com>
2023-08-18 15:51:53 +0800

    fixed an error in backend

--- src/main/java/com/controlcenter/controlcenter/dao/UserDAO.java
+++ src/main/java/com/controlcenter/controlcenter/dao/UserDAO.java
@@ -1,16 +1,14 @@
 package com.controlcenter.controlcenter.dao;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
 import com.controlcenter.controlcenter.model.User;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserDao {
-    List<User> findAll();
-    User getUserById(Long id);
-    void insertUser(User user);
-    void insertUserBatch(List<User> users);
-    User getUserByUsername(User user);
+  List<User> findAll();
+  User getUserById(Long id);
+  void insertUser(User user);
+  void insertUserBatch(List<User> users);
+  User getUserByUsername(User user);
 }
