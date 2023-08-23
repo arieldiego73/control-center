16d152600857c599a591e87ccf2711634f04c33a
Ricky Galpo <rickygalpo7777@gmail.com>
2023-08-23 15:06:52 +0800

    Added department service, controller, dao, mapper, model.

---
+++ src/main/java/com/controlcenter/controlcenter/controller/DepartmentController.java
@@ -0,0 +1,43 @@
+package com.controlcenter.controlcenter.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.controlcenter.controlcenter.model.Department;
+import com.controlcenter.controlcenter.service.DepartmentService;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+    
+    @Autowired
+    public DepartmentService departmentService;
+
+    @GetMapping("/all")
+    public List<Department> getAllDepartment() {
+        return departmentService.getAllDepartment();
+    }
+
+    @PostMapping("/add")
+    public String addDepartment(@RequestBody Department department) {
+        return departmentService.addDepartment(department);
+    }
+
+    @PutMapping("/edit/{id}")
+    public String editDepartmentInfo(@PathVariable String id, @RequestBody Department department) {
+        return departmentService.editDepartmentInfo(id, department);
+    }
+
+    @PutMapping("delete/{id}")
+    public String logicalDeleteDepartment(@PathVariable String id) {
+        return departmentService.logicalDeleteDepartment(id);
+    }
+}
