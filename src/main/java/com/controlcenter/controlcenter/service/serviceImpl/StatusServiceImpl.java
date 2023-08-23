2ecac42868a8bce9fa3fc4872ad37b479b171746
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-08-23 21:11:13 +0800

    Updated Status COntroller, Service, Dao, Mapper and Impl. 100%

---
+++ src/main/java/com/controlcenter/controlcenter/service/serviceImpl/StatusServiceImpl.java
@@ -0,0 +1,61 @@
+package com.controlcenter.controlcenter.service.serviceImpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.controlcenter.controlcenter.dao.StatusDao;
+import com.controlcenter.controlcenter.model.Status;
+import com.controlcenter.controlcenter.service.StatusService;
+
+@Service
+public class StatusServiceImpl implements StatusService{
+
+    @Autowired 
+    public StatusDao statusDao;
+
+    @Override
+    public List<Status> getAllStatus(){
+        return statusDao.getAllStatus();
+    }
+
+    @Override
+    public String addStatus(Status status){
+        try{
+            statusDao.addStatus(status);
+            return "Status Added Successfully";
+        } catch(Exception e){
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public String editStatusInfo(String code, Status status){
+        try{
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("code", code);
+            paramMap.put("status", status);
+
+            statusDao.editStatusInfo(paramMap);
+
+            return "Status Info Edited Successfully";
+        } catch (Exception e){
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public String logicalDeleteStatus(String code){
+        try{
+            statusDao.logicalDeleteStatus(code);
+
+            return "Status Deleted Successfully";
+        } catch (Exception e){
+            return e.getMessage();
+        }
+    }
+    
+}
