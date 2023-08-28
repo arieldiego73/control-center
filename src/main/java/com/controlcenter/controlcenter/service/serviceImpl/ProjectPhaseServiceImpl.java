98c6bd860fd7250c8247ed2c36b9ac1769518e99
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-08-29 07:49:53 +0800

    Project Phase Added

---
+++ src/main/java/com/controlcenter/controlcenter/service/serviceImpl/ProjectPhaseServiceImpl.java
@@ -0,0 +1,59 @@
+package com.controlcenter.controlcenter.service.serviceImpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.controlcenter.controlcenter.dao.ProjectPhaseDao;
+import com.controlcenter.controlcenter.model.ProjectPhase;
+import com.controlcenter.controlcenter.service.ProjectPhaseService;
+
+@Service
+public class ProjectPhaseServiceImpl implements ProjectPhaseService {
+
+    @Autowired
+    ProjectPhaseDao projectPhaseDao;
+
+    @Override
+    public List<ProjectPhase> getAllProjectPhase(){
+        return projectPhaseDao.getAllProjectPhase();
+    }
+
+    @Override
+    public String addProjectPhase(ProjectPhase projectPhase){
+        try {
+            projectPhaseDao.addProjectPhase(projectPhase);
+            return "Project Phase Added Successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public String editProjectPhase(String id, ProjectPhase projectPhase){
+        try {
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("id", id);
+            paramMap.put("projectPhase", projectPhase);
+
+            projectPhaseDao.editProjectPhase(paramMap);
+            return "Project Phase Edited Successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public String logicalDeleteProjectPhase(String id){
+        try {
+            projectPhaseDao.logicalDeleteProjectPhase(id);
+            return "Project Phase Deleted Successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+    
+}
