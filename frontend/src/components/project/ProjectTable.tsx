cbbbd9889e0820633dec25c4b88274b3f2cf89ea
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2023-08-29 08:12:41 +0800

    fixed projTable

--- frontend/src/components/project/ProjectTable.tsx
+++ frontend/src/components/project/ProjectTable.tsx
@@ -101,30 +101,12 @@ export default function ProjectTable() {
 					</Table>
 				</TableContainer>
 
-<<<<<<< HEAD
-			{/* Popup */}
-=======
 				{/* Popup */}
->>>>>>> bdd4e71149d91910e4171a5119125fb098b7604a
 				<Dialog
 					open={open}
 					onClose={handleClose}
 					aria-describedby="alert-dialog-slide-description"
 				>
-<<<<<<< HEAD
-					<DialogTitle><FontAwesomeIcon style={{paddingRight:'1%'}} icon={faUser} size="1x" color="black"  />{"Members"}</DialogTitle>
-					<DialogContent>
-								<MembersTable />
-					</DialogContent>
-					<DialogActions>
-					<Button onClick={handleClose}>Close</Button>
-					</DialogActions>
-				</Dialog>
-			</div>
-		);
-		
-	}
-=======
 					<DialogTitle>
 						<FontAwesomeIcon icon={faUser} size="1x" color="black" />
 						{"Members"}
@@ -150,4 +132,3 @@ export default function ProjectTable() {
 	);
 
 }
->>>>>>> bdd4e71149d91910e4171a5119125fb098b7604a
