84c0b51931e7bce2bb635011c4cc7c9f4bca128a
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2023-08-29 11:22:05 +0800

    Created UserDetails page (progress 1%)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import RoleHandler from "./pages/RoleHandler";
 // import NewProj from "./components/project/new_project/NewProj";
 import NewProjHandler from "./pages/NewProjectHandler";
 import test from "./components/project/new_project/test";
+import UserDetails from "./components/user/UserDetails";
 
 function App() {
 	return (
@@ -23,6 +24,7 @@ function App() {
 				<Route path="/role" Component={RoleHandler} />
 				<Route path="/NewProj" Component={NewProjHandler} />
 				<Route path="/test" Component={test} />
+				<Route path="/User/:name" Component={UserDetails} />
 			</Routes>
 		</BrowserRouter>
 	);
