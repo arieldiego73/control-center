51f4f2b9d393dfc6c5f93bc68abb2c19f8f06e29
Ricky Galpo <rickygalpo7777@gmail.com>
2023-08-30 12:33:09 +0800

    Implemented Input and Output model on DevPhase

---
+++ src/main/java/com/controlcenter/controlcenter/model/DevPhaseInput.java
@@ -0,0 +1,11 @@
+package com.controlcenter.controlcenter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DevPhaseInput {
+    private String dev_phase_name;
+    private String dev_phase_sh_name;
+}
