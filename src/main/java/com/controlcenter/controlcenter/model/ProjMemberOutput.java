03c993823ab1226fcae27c304b5530b70a73b1be
Ricky Galpo <rickygalpo7777@gmail.com>
2023-08-30 13:45:24 +0800

    Implemented Input and Output model on ProjMember

---
+++ src/main/java/com/controlcenter/controlcenter/model/ProjMemberOutput.java
@@ -0,0 +1,12 @@
+package com.controlcenter.controlcenter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProjMemberOutput {
+    private Long up_role_id;
+    private int upid;
+    private int role_id;
+}
