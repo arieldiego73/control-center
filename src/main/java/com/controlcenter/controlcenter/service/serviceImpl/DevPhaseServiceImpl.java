884ef1bc0ffdf2ab8f4396cd476260f109493793
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-08-31 15:34:53 +0800

    Activity log for Devphase

--- src/main/java/com/controlcenter/controlcenter/service/serviceImpl/DevPhaseServiceImpl.java
+++ src/main/java/com/controlcenter/controlcenter/service/serviceImpl/DevPhaseServiceImpl.java
@@ -1,5 +1,7 @@
 package com.controlcenter.controlcenter.service.serviceImpl;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +9,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.controlcenter.controlcenter.dao.ActivityLogDao;
 import com.controlcenter.controlcenter.dao.DevPhaseDao;
+import com.controlcenter.controlcenter.model.ActivityLogInput;
 import com.controlcenter.controlcenter.model.DevPhaseInput;
 import com.controlcenter.controlcenter.model.DevPhaseOutput;
 import com.controlcenter.controlcenter.service.DevPhaseService;
@@ -18,6 +22,9 @@ public class DevPhaseServiceImpl implements DevPhaseService {
     @Autowired
     public DevPhaseDao devPhaseDao;
 
+    @Autowired
+    public ActivityLogDao activityLogDao;
+
     @Override
     public List<DevPhaseOutput> getAllDevPhase(){
         return devPhaseDao.getAllDevPhase();
@@ -27,6 +34,20 @@ public class DevPhaseServiceImpl implements DevPhaseService {
     public String addDevPhase(DevPhaseInput devPhase) {
         try {
             devPhaseDao.addDevPhase(devPhase);
+
+            ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Added a dev phase.");
+
+            long currentTimeMillis = System.currentTimeMillis();
+            // Convert milliseconds to a human-readable date and time format
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String formattedDate = sdf.format(new Date(currentTimeMillis));
+            activityLogInput.setLog_date(formattedDate);
+            // add the activity log
+            activityLogDao.addActivityLog(activityLogInput);
+            
             return "Development Phase Added Successfully";
         } catch (Exception e) {
             return e.getMessage();
