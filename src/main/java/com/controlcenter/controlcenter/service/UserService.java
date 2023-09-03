9ea8e95d30dc74fbb72da33811b84e9022d0bea3
Ricky Galpo <rickygalpo7777@gmail.com>
2023-09-04 07:52:01 +0800

    fixed model for getUserByUsername and added a getUsername api

--- src/main/java/com/controlcenter/controlcenter/service/UserService.java
+++ src/main/java/com/controlcenter/controlcenter/service/UserService.java
@@ -9,12 +9,13 @@ import java.util.List;
 public interface UserService {
   public List<UserOutput> findAll();
 
-  public UserInput getUserById(Long id);
+  public UserOutput getUserById(Long id);
 
   public String insertUser(UserInput user);
 
-  // public String insertUserBatch(List<User> users);
-  public UserInput getUserByUsername(UserInput user);
+  public UserOutput getUserByUsername(UserOutput username);
+
+  public UserOutput getUsername(String username);
 
   public String addAccount(Account account);
 }
