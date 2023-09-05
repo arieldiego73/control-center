e8b26c0a92de81ba032c80dd5de69936e7c821b3
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-05 10:24:22 +0800

    Added Validation in ProjectPhase Input

--- src/main/java/com/controlcenter/controlcenter/model/ProjectPhaseInput.java
+++ src/main/java/com/controlcenter/controlcenter/model/ProjectPhaseInput.java
@@ -1,10 +1,19 @@
 package com.controlcenter.controlcenter.model;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProjectPhaseInput {
+
+    @NotNull
+    @Size(max = 9)
     private int dev_phase_id;
+
+    @NotNull
+    @Size(max = 9)
     private int proj_id;
 }
