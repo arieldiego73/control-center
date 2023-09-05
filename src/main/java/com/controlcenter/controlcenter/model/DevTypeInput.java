7ac47c6efddd1ab14f59284e626fa8808c0af576
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-05 09:50:53 +0800

    Added Validation in DevType Input

--- src/main/java/com/controlcenter/controlcenter/model/DevTypeInput.java
+++ src/main/java/com/controlcenter/controlcenter/model/DevTypeInput.java
@@ -1,11 +1,20 @@
 package com.controlcenter.controlcenter.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class DevTypeInput {
+
+    @NotNull
+    @Size(max = 150)
     private String dev_type_name;
+
+    @NotNull
+    @Size(max = 50)
     private String dev_type_sh_name;
 }
