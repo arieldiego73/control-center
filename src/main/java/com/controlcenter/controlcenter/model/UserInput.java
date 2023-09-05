2cb7077ea62d0472fdbf9d543962d7eb6d0e65ed
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-09-05 09:40:18 +0800

    Validation for User Input

--- src/main/java/com/controlcenter/controlcenter/model/UserInput.java
+++ src/main/java/com/controlcenter/controlcenter/model/UserInput.java
@@ -1,5 +1,8 @@
 package com.controlcenter.controlcenter.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,13 +11,35 @@ import lombok.Setter;
 public class UserInput {
 
   //User Model
+  @NotNull
+  @Size(max = 50)
   private String emp_id;
+
+  @NotNull
+  @Size(max = 20)
   private String username;
+
+  @NotNull
+  @Size(max = 255)
   private String password;
+  
+  @NotNull
   private int position_id;
+
+  @NotNull
   private int dept_id;
+
+  @NotNull
   private int section_id;
+
+  @NotNull
+  @Size(max = 10)
   private String status_code;
+
+  @NotNull
   private int role_id;
+
+  @NotNull
+  @Size(max = 255)
   private String img_src;
 }
