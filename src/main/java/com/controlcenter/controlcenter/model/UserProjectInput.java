a55be8cd516c66fb852890aecae289d1ec2aa95f
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-09-05 09:25:41 +0800

    Validation for User project

--- src/main/java/com/controlcenter/controlcenter/model/UserProjectInput.java
+++ src/main/java/com/controlcenter/controlcenter/model/UserProjectInput.java
@@ -1,11 +1,19 @@
 package com.controlcenter.controlcenter.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserProjectInput {
+
+    @NotNull
+    @Size(max = 50)
     private String emp_id;
+
+    @NotNull
     private int proj_id;
 }
