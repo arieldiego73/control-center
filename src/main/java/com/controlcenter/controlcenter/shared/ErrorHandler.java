9e8a727a9ec17f045e4e031f3308f968b378e14c
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-06 08:53:21 +0800

    updated error handler.java

--- src/main/java/com/controlcenter/controlcenter/shared/ErrorHandler.java
+++ src/main/java/com/controlcenter/controlcenter/shared/ErrorHandler.java
@@ -4,7 +4,13 @@ import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ErrorHandler {
+
+    @Bean
     public <T> String getErrors(Set<ConstraintViolation<T>> errors) {
         StringBuilder errorMessage = new StringBuilder("Error Message/s:\n");
                 //loop all the errors encountered then compile them into a string:
