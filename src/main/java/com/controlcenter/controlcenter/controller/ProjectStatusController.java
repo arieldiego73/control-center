2f8e0f92d4d57cedb0cfa6f9e143902f4a98a5b0
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-09-06 15:41:07 +0800

    Validation for Project status

--- src/main/java/com/controlcenter/controlcenter/controller/ProjectStatusController.java
+++ src/main/java/com/controlcenter/controlcenter/controller/ProjectStatusController.java
@@ -1,6 +1,12 @@
 package com.controlcenter.controlcenter.controller;
 
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.controlcenter.controlcenter.model.ProjectStatusInput;
 import com.controlcenter.controlcenter.model.ProjectStatusOutput;
 import com.controlcenter.controlcenter.service.ProjectStatusService;
+import com.controlcenter.controlcenter.shared.ErrorHandler;
 
 @RestController
 @RequestMapping("/project-status")
 public class ProjectStatusController {
+
     @Autowired
     public ProjectStatusService projectStatusService;
+
+    @Autowired
+    private ErrorHandler errorHandler;
 
     @GetMapping("/all")
     public List<ProjectStatusOutput> getAllProjectStatus() {
@@ -28,12 +39,30 @@ public class ProjectStatusController {
 
     @PostMapping("/add")
     public String addProjectStatus(@RequestBody ProjectStatusInput projectStatus){
-        return projectStatusService.addProjectStatus(projectStatus);
+        //For Validation
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<ProjectStatusInput>> errors = validator.validate(projectStatus);
+            //Error Handling
+            if (errors.size() > 0) { //checks the errors from validator
+                return errorHandler.getErrors(errors);
+            }else{
+                return projectStatusService.addProjectStatus(projectStatus);
+            }
     }
 
     @PutMapping("/edit/{id}")
     public String editProjectStatus(@PathVariable String id, @RequestBody ProjectStatusInput projectStatus) {
-        return projectStatusService.editProjectStatus(id, projectStatus);
+        //For Validation
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<ProjectStatusInput>> errors = validator.validate(projectStatus);
+            //Error Handling
+            if (errors.size() > 0) { //checks the errors from validator
+                return errorHandler.getErrors(errors);
+            }else{
+                return projectStatusService.addProjectStatus(projectStatus);
+            }
     }
 
     @PutMapping("/delete/{id}")
