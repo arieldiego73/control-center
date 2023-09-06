153d46137d1094a244383ab4b82eb89f1e31888e
Ricky Galpo <rickygalpo7777@gmail.com>
2023-09-06 15:01:42 +0800

    Changed return type of user API to ResponseEntity

--- src/main/java/com/controlcenter/controlcenter/controller/UserController.java
+++ src/main/java/com/controlcenter/controlcenter/controller/UserController.java
@@ -35,27 +35,26 @@ public class UserController {
     return userService.findAll();
   }
 
-  // @GetMapping("/info/{id}")
-  // public ResponseEntity<UserInfoOutput> getUserById(@PathVariable Long id) {
-  //   UserInfoOutput user = userService.getUserById(id);
-  //   return ResponseEntity.ok(user);
-  // }
-
-  @PostMapping("/create")
-  public String createUser(@RequestBody UserInput user) {
-    UserInput userHashed = user;
-    userHashed.setPassword(passEnc.encode(user.getPassword()));
-
-    try {
-      userService.insertUser(userHashed);
-      return "User created successfully";
-    } catch (Exception e) {
-      return e.getMessage();
-    }
+  @GetMapping("/info/{id}")
+  public ResponseEntity<UserInfoOutput> getUserById(@PathVariable String id) {
+    return userService.getUserById(id);
   }
 
+  // @PostMapping("/create")
+  // public String createUser(@RequestBody UserInput user) {
+  //   UserInput userHashed = user;
+  //   userHashed.setPassword(passEnc.encode(user.getPassword()));
+
+  //   try {
+  //     userService.insertUser(userHashed);
+  //     return "User created successfully";
+  //   } catch (Exception e) {
+  //     return e.getMessage();
+  //   }
+  // }
+
   @PostMapping("/create-account")
-  public String addAccount(@RequestBody Account account) {
+  public ResponseEntity<Account> addAccount(@RequestBody Account account) {
     return userService.addAccount(account);
   }
   // @PostMapping("/createBatch")
@@ -87,12 +86,12 @@ public class UserController {
   // }
 
   @PostMapping("/login")
-  public HashMap<String, Object> getLoggedInUser(@RequestBody UserOutput user) {
+  public ResponseEntity<UserOutput> getLoggedInUser(@RequestBody UserOutput user) {
     return userService.getLoggedInUser(user);
   }
 
   @GetMapping("/{username}")
-  public UserOutput getUserByUsername(@PathVariable String username) {
+  public ResponseEntity<UserOutput> getUserByUsername(@PathVariable String username) {
     return userService.getUsername(username);
   }
 }
