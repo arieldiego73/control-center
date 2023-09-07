10c56136afe1696d1d1aca5a15f8581d8a1f8e0d
Ariel Diego <arieldiego73@gmail.com>
2023-09-07 11:13:05 +0800

    fixed dev phase service implementation

--- src/main/java/com/controlcenter/controlcenter/service/serviceImpl/DevPhaseServiceImpl.java
+++ src/main/java/com/controlcenter/controlcenter/service/serviceImpl/DevPhaseServiceImpl.java
@@ -1,7 +1,5 @@
 package com.controlcenter.controlcenter.service.serviceImpl;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import com.controlcenter.controlcenter.dao.ActivityLogDao;
 import com.controlcenter.controlcenter.dao.DevPhaseDao;
-import com.controlcenter.controlcenter.model.ActivityLogInput;
 import com.controlcenter.controlcenter.model.DevPhaseInput;
 import com.controlcenter.controlcenter.model.DevPhaseOutput;
 import com.controlcenter.controlcenter.service.DevPhaseService;
@@ -40,41 +37,10 @@ public class DevPhaseServiceImpl implements DevPhaseService {
     
     @Override
     public ResponseEntity<List<DevPhaseOutput>> addDevPhase(DevPhaseInput devPhase) {
-        String devPhaseName = devPhase.getDev_phase_name();
-        String devPhaseShortName = devPhase.getDev_phase_sh_name();
-
         try {
-            if(devPhaseName != null && devPhaseShortName != null) {
-                if((devPhaseName.length() > 150 || devPhaseName.length() < 1) || (devPhaseShortName.length() > 50 || devPhaseShortName.length() < 1)) {
-                    return "The length of the data entered does not reach the specified length.";
-                } else {
-                    devPhaseDao.addDevPhase(devPhase);
-
-                    ActivityLogInput activityLogInput = new ActivityLogInput();
-
-                    activityLogInput.setEmp_id("101"); //current logged user dapat
-                    activityLogInput.setLog_desc("Added a dev phase.");
-
-                    long currentTimeMillis = System.currentTimeMillis();
-                    // Convert milliseconds to a human-readable date and time format
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    String formattedDate = sdf.format(new Date(currentTimeMillis));
-                    activityLogInput.setLog_date(formattedDate);
-                    // add the activity log
-                    activityLogDao.addActivityLog(activityLogInput);
-        
-                    List<DevPhaseOutput> devPhaseList = devPhaseDao.getAllDevPhase();
+            devPhaseDao.addDevPhase(devPhase);
+            List<DevPhaseOutput> devPhaseList = devPhaseDao.getAllDevPhase();
             return ResponseEntity.ok(devPhaseList);
-                }
-            } else {
-                if(devPhaseName == null && devPhaseShortName != null){
-                    return "Development Phase name should not be empty.";
-                } else if(devPhaseShortName == null && devPhaseName != null){
-                    return "Development Phase short name should not be empty.";
-                } else{
-                    return "All fields should not be empty.";
-                }
-            }
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(nullDevPhase);
         }
@@ -82,32 +48,15 @@ public class DevPhaseServiceImpl implements DevPhaseService {
 
     @Override 
     public ResponseEntity<List<DevPhaseOutput>> editDevPhaseInfo(String id, DevPhaseInput devPhase) {
-        String devPhaseName = devPhase.getDev_phase_name();
-        String devPhaseShortName = devPhase.getDev_phase_sh_name();
-
         try {
-            if(devPhaseName != null && devPhaseShortName != null) {
-                if((devPhaseName.length() > 150 || devPhaseName.length() < 1) || (devPhaseShortName.length() > 50 || devPhaseShortName.length() < 1)) {
-                    return "The length of the data entered does not reach the specified length.";
-                } else {
-                    Map<String, Object> paramMap = new HashMap<>();
-                    paramMap.put("id", id);
-                    paramMap.put("devPhase", devPhase);
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("id", id);
+            paramMap.put("devPhase", devPhase);
 
-                    devPhaseDao.editDevPhaseInfo(paramMap);
+            devPhaseDao.editDevPhaseInfo(paramMap);
 
-                    List<DevPhaseOutput> devPhaseList = devPhaseDao.getAllDevPhase();
+            List<DevPhaseOutput> devPhaseList = devPhaseDao.getAllDevPhase();
             return ResponseEntity.ok(devPhaseList);
-                }
-            } else {
-                if(devPhaseName == null && devPhaseShortName != null){
-                    return "Development Phase name should not be empty.";
-                } else if(devPhaseShortName == null && devPhaseName != null){
-                    return "Development Phase short name should not be empty.";
-                } else{
-                    return "All fields should not be empty.";
-                }
-            }
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(nullDevPhase);
         }
