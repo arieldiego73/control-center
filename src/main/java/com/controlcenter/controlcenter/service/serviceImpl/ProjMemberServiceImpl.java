4667c7a05367638496a22af1dd1cbc9d72c0d4df
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-09-11 10:31:32 +0800

    activitylog for project member: add, edit, delete, restore

--- src/main/java/com/controlcenter/controlcenter/service/serviceImpl/ProjMemberServiceImpl.java
+++ src/main/java/com/controlcenter/controlcenter/service/serviceImpl/ProjMemberServiceImpl.java
@@ -7,10 +7,13 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.controlcenter.controlcenter.dao.ActivityLogDao;
 import com.controlcenter.controlcenter.dao.ProjMemberDao;
+import com.controlcenter.controlcenter.model.ActivityLogInput;
 import com.controlcenter.controlcenter.model.ProjMemberInput;
 import com.controlcenter.controlcenter.model.ProjMemberOutput;
 import com.controlcenter.controlcenter.service.ProjMemberService;
+import com.controlcenter.controlcenter.shared.TimeFormatter;
 
 
 @Service
@@ -19,6 +22,12 @@ public class ProjMemberServiceImpl implements ProjMemberService{
     @Autowired
     public ProjMemberDao projMemberDao;
 
+    @Autowired
+    public TimeFormatter timeFormatter;
+
+    @Autowired
+    public ActivityLogDao activityLogDao;
+    
     @Override
     public List<ProjMemberOutput> getAllProjMember(){
         return projMemberDao.getAllProjMember();
@@ -28,6 +37,18 @@ public class ProjMemberServiceImpl implements ProjMemberService{
     public String addProjMember(ProjMemberInput projMember) {
         try {
             projMemberDao.addProjMember(projMember);
+
+            //Activitylog
+            ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Added a project member.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+            
             return "Project Member Added Successfully";
         } catch (Exception e) {
             return e.getMessage();
@@ -43,6 +64,17 @@ public class ProjMemberServiceImpl implements ProjMemberService{
 
             projMemberDao.editProjMemberInfo(paramMap);
 
+            //Activitylog
+            ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Edited a project member.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+
             return "Project Member Edited Successfully";
         } catch (Exception e) {
             return e.getMessage();
@@ -53,6 +85,18 @@ public class ProjMemberServiceImpl implements ProjMemberService{
     public String logicalDeleteProjMember(String id) {
         try {
             projMemberDao.logicalDeleteProjMember(id);
+
+            //Activitylog
+            ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Deleted a project member.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+
             return "Project Member Deleted Successfully";
         } catch (Exception e) {
             return e.getMessage();
@@ -63,6 +107,18 @@ public class ProjMemberServiceImpl implements ProjMemberService{
     public String restoreProjMember(String id) {
         try {
             projMemberDao.restoreProjMember(id);
+
+            //Activitylog
+            ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Restored a project member.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+
             return "Project Member Restored Successfully";
         } catch (Exception e) {
             return e.getMessage();
