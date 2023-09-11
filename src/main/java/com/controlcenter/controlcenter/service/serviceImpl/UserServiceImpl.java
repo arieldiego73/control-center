04735d656531e4d7ccc397c9afd0483defbdb932
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-09-11 14:37:30 +0800

    Activitylog for user: create, login, logout

--- src/main/java/com/controlcenter/controlcenter/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/controlcenter/controlcenter/service/serviceImpl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.controlcenter.controlcenter.service.serviceImpl;
 
+import com.controlcenter.controlcenter.dao.ActivityLogDao;
 import com.controlcenter.controlcenter.dao.PersonalInfoDao;
 import com.controlcenter.controlcenter.dao.UserDao;
 import com.controlcenter.controlcenter.model.Account;
+import com.controlcenter.controlcenter.model.ActivityLogInput;
 import com.controlcenter.controlcenter.model.PersonalInfoInput;
 import com.controlcenter.controlcenter.model.UserInfoOutput;
 import com.controlcenter.controlcenter.model.UserInput;
 import com.controlcenter.controlcenter.model.UserOutput;
 import com.controlcenter.controlcenter.model.UserTable;
 import com.controlcenter.controlcenter.service.UserService;
+import com.controlcenter.controlcenter.shared.TimeFormatter;
 
 // import java.util.Collections;
 import java.util.List;
@@ -28,6 +31,12 @@ public class UserServiceImpl implements UserService{
 
   @Autowired
   public UserDao userDao;
+
+  @Autowired
+  public TimeFormatter timeFormatter;
+
+  @Autowired 
+  public ActivityLogDao activityLogDao;
 
   @Autowired 
   public PersonalInfoDao personalInfoDao;
@@ -96,6 +105,17 @@ public class UserServiceImpl implements UserService{
     try {
       userDao.insertUser(user);
       personalInfoDao.addPersonalInfo(personalInfo);
+
+      ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Added a user.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+
       return ResponseEntity.ok(account);
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -164,8 +184,28 @@ public class UserServiceImpl implements UserService{
         users.setRole_id(users.getRole_id());
         users.setImg_src(users.getImg_src());
 
+        ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Login successfully.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+
         return ResponseEntity.ok(users);
       } else {
+        ActivityLogInput activityLogInput = new ActivityLogInput();
+
+            activityLogInput.setEmp_id("101"); //current logged user dapat
+            activityLogInput.setLog_desc("Login unsuccessful.");
+
+            Long currentTimeMillis = System.currentTimeMillis();
+            //add the activity log
+            activityLogInput.setLog_date(timeFormatter.formatTime(currentTimeMillis));
+            activityLogDao.addActivityLog(activityLogInput);
+
         return ResponseEntity.badRequest().body(users);
       }
     } else {
