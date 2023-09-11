3ed2ca0647895803f3cc789d6513459319a22cb5
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-12 07:12:20 +0800

    comment bean import on security config

--- src/main/java/com/controlcenter/controlcenter/config/SecurityConfig.java
+++ src/main/java/com/controlcenter/controlcenter/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.controlcenter.controlcenter.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 // import org.springframework.security.authentication.AuthenticationManager;
 // import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
