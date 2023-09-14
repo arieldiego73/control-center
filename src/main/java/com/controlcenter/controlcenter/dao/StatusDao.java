085efc0b510732dc9405089b0bf86ba27f80d6ad
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-14 14:29:04 +0800

    added batch delete in status

--- src/main/java/com/controlcenter/controlcenter/dao/StatusDao.java
+++ src/main/java/com/controlcenter/controlcenter/dao/StatusDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.controlcenter.controlcenter.model.StatusInput;
 import com.controlcenter.controlcenter.model.StatusOutput;
@@ -11,10 +12,16 @@ import com.controlcenter.controlcenter.model.StatusOutput;
 @Mapper
 public interface StatusDao {
     List<StatusOutput> getAllStatus();
+
     StatusOutput getStatusById(String id);
+
     void addStatus(StatusInput status);
+
     void editStatusInfo(Map<String, Object> paramMap);
+
     void logicalDeleteStatus(String code);
+
+    void deleteMultipleStatus(@Param("ids") List<String> ids);
+
     void restoreStatus(String code);
 }
-
