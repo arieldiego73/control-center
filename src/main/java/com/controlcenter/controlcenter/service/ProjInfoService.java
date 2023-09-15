b724631de0b5309ba53ba1fc0c55640dcb7bbacf
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-15 10:38:07 +0800

    updated record validation in proj info

--- src/main/java/com/controlcenter/controlcenter/service/ProjInfoService.java
+++ src/main/java/com/controlcenter/controlcenter/service/ProjInfoService.java
@@ -10,7 +10,7 @@ public interface ProjInfoService {
     public List<ProjInfoOutput> getAllProjInfo();
     public ProjInfoOutput getProjInfoById(String id);
     public String addProjInfo(ProjInfoInput projInfo);
-    public String editProjInfoInfo(String id, ProjInfoInput projInfo);
+    public String editProjInfo(String id, ProjInfoInput projInfo);
     public String logicalDeleteProjInfo(String id);
     public String restoreProjInfo(String id);
 
