08ed25877aa22c8cf93365d95920de53e1f27024
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2023-09-15 09:45:47 +0800

    dynamic breadcrumbs and added sample user image

--- frontend/src/pages/Userhandler.tsx
+++ frontend/src/pages/Userhandler.tsx
@@ -1,12 +1,21 @@
+import React from "react";
 import NavigationHandler from "./NavigationHandler";
 import Userpage from "../components/user/Userpage";
+import HomeIcon from '@mui/icons-material/Home';
+
 
 export default function Userhandler() {
-  const pageTitle = "USER PAGEEEEE";
+
+  const pageTitle = "USER PAGE";
+
+  const breadcrumbs = [
+    { icon: <HomeIcon style={{height:"20px", marginTop:"5px"}}/>,  to: "/dashboard"  }, // Example breadcrumb data
+    { label: "User", to: "/user" },
+  ];
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <NavigationHandler pageTitle={pageTitle} /> {/* Pass the pageTitle prop here */}
+      <NavigationHandler pageTitle={pageTitle} breadcrumbs={breadcrumbs} /> {/* Pass the pageTitle and breadcrumbs prop here */}
       <Userpage />
     </div>
   );
