d0f040c25e3b4c157b44a198064cf8f91af99024
Kekingkyawz19 <charlene.valdez19@gmail.com>
2023-09-19 14:49:05 +0800

    complete all the bread crumbs and title of each pages

--- frontend/src/pages/DevelopmentTypeHandler.tsx
+++ frontend/src/pages/DevelopmentTypeHandler.tsx
@@ -1,11 +1,19 @@
 import DevelopmentType from "../components/development_type/DevelopmentType";
 import NavigationHandler from "./NavigationHandler";
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function DevelopmentTypeHandler (){
+    const pageTitle = "DEVELOPMENT TYPE";
+
+	const breadcrumbs = [
+		{ icon: <HomeIcon style={{ height: "20px", marginTop: "5px" }} />, to: "/dashboard" }, 
+		{ label: "Development Type", to: "/devType" },
+	];
+    
     return(
         <div style={{ flexDirection: "column", display: "flex" }}>
 		
-        <NavigationHandler/> 
+        <NavigationHandler pageTitle={pageTitle} breadcrumbs={breadcrumbs} /> 
          <DevelopmentType/>
   
 		</div>
