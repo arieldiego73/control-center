f27b44dfadf28f1ceca8565e32f8ec8651c4830e
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2023-09-19 08:53:03 +0800

    Fixed dynamic breadcrumbs and title pages

--- frontend/src/pages/RoleHandler.tsx
+++ frontend/src/pages/RoleHandler.tsx
@@ -1,10 +1,20 @@
 import Role from "../components/role/Role";
 import NavigationHandler from "./NavigationHandler";
+import HomeIcon from '@mui/icons-material/Home';
+
 
 export default function RoleHandler() {
+
+	const pageTitle = "ROLES";
+
+	const breadcrumbs = [
+		{ icon: <HomeIcon style={{ height: "20px", marginTop: "5px" }} />, to: "/dashboard" }, // Example breadcrumb data
+		{ label: "Roles", to: "/role" },
+	];
+
 	return (
 		<div style={{ flexDirection: "column", display: "flex" }}>
-				<NavigationHandler />
+			<NavigationHandler pageTitle={pageTitle} breadcrumbs={breadcrumbs} /> {/* Pass the pageTitle and breadcrumbs prop here */}
 				<Role />
 		</div>
 	);
