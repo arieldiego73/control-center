957a7375c59638c3a564633299069f5a9c91f3cf
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-09-26 11:29:35 +0800

    added batch delete in dev type

--- src/main/java/com/controlcenter/controlcenter/service/DevTypeService.java
+++ src/main/java/com/controlcenter/controlcenter/service/DevTypeService.java
@@ -3,6 +3,7 @@ package com.controlcenter.controlcenter.service;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.controlcenter.controlcenter.model.DevTypeInput;
 import com.controlcenter.controlcenter.model.DevTypeOutput;
@@ -16,5 +17,6 @@ public interface DevTypeService {
     public String addDevType(DevTypeInput devType, String emp_id);
     public String editDevTypeInfo(String id, DevTypeInput devType, String emp_id);
     public String logicalDeleteDevType(String id, String emp_id);
+    public String deleteMultipleDevType(@RequestParam List<Long> ids, String emp_id);
     public String restoreDevType(String id);
 }
