44a45f6fc3788ae85859805ed275110e6e11fae5
Allona Fabre <102402384+yona519@users.noreply.github.com>
2023-10-04 08:14:12 +0800

    add batch delete in project info

--- src/main/java/com/controlcenter/controlcenter/service/ProjInfoService.java
+++ src/main/java/com/controlcenter/controlcenter/service/ProjInfoService.java
@@ -3,17 +3,25 @@ package com.controlcenter.controlcenter.service;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.controlcenter.controlcenter.model.ProjInfoInput;
 import com.controlcenter.controlcenter.model.ProjInfoOutput;
 
 public interface ProjInfoService {
-    
+
     public ResponseEntity<List<ProjInfoOutput>> getAllProjInfo();
+
     public ProjInfoOutput getProjInfoById(String id);
+
     public String addProjInfo(ProjInfoInput projInfo, String emp_id);
+
     public String editProjInfo(String id, ProjInfoInput projInfo, String emp_id);
+
     public String logicalDeleteProjInfo(String id, String emp_id);
+
     public String restoreProjInfo(String id, String emp_id);
+
+    public String deleteMultipleProjInfo(@RequestParam List<Long> ids, String emp_id);
 
 }
