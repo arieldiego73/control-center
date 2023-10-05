41d4e3c9a32871273ed1d65ef8af704c7cca049d
Kekingkyawz19 <142292496+Kekingkyawz19@users.noreply.github.com>
2023-10-05 10:27:21 +0800

    delete and multiple delete of User Table

--- frontend/src/redux/saga/userSaga.ts
+++ frontend/src/redux/saga/userSaga.ts
@@ -221,7 +221,7 @@ const apiBatchDelete = async (batchId: Set<GridRowId>): Promise<any> => {
 	try {
 		const params = new URLSearchParams();
 		batchId.forEach((id) => {
-			params.append("id", id.toString());
+			params.append("ids", id.toString());
 		});
 		const url = `http://localhost:8080/user/delete-multiple?${params}`
 		return axios.put(url);
