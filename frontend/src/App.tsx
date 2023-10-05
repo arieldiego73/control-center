5384de59bc956bf62ca97310a479089cd69562bb
Kekingkyawz19 <142292496+Kekingkyawz19@users.noreply.github.com>
2023-10-06 07:28:20 +0800

    FOR USER RESTRICTION (DUMMY DATA ONLY)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,117 @@
+// import "./App.css";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import LoginPage from "./pages/LoginPage";
+// import Userhandler from "./pages/Userhandler";
+// import CreateUserHandler from "./pages/CreateUserHandler";
+// import ProjectHandler from "./pages/ProjectHandler";
+// import RoleHandler from "./pages/RoleHandler";
+// // import NewProj from "./components/project/new_project/NewProj";
+// import NewProjHandler from "./pages/NewProjectHandler";
+// import AddMemberTable from "./components/project/AddMemberTable";
+// // import UserDetails from "./components/user/edit/EditUser";
+// import EditUserHandler from "./pages/EditUserHandler";
+// import EditProjectHandler from "./pages/EditProjectHandler"
+// import DevelopmentPhaseHandler from "./pages/DevelopmentPhaseHandler";
+// import ProjectStatusHandler from "./pages/ProjectStatusHandler";
+// import DashboardHandler from "./pages/DashboardHandler";
+// import ActivityLogDashboardTable from "./components/dashboard/ActivityLogDashboardTable";
+// import EmployeeStatusHandler from "./pages/EmployeeStatusHandler";
+// import EmployeePositionHandler from "./pages/EmployeePositionHandler";
+// import DepartmentHandler from "./pages/DepartmentHandler";
+// import BusinessUnitHandler from "./pages/BusinessUnitHandler";
+// import TechnologyHandler from "./pages/TechnologyHandler";
+// import test2 from "./components/test/test2";
+// import Sidenav from "./components/newNav/SideNav"
+// import Topnav from "./components/newNav/Navigations"
+// import Navigations from "./pages/NavigationHandler"
+// import UserGraph from "./components/dashboard/user_graph/UserGraph";
+// import DevelopmentTypeHandler from "./pages/DevelopmentTypeHandler";
+// import TestHandler from "./pages/TestHandler";
+// import ClientHandler from "./pages/ClientHandler";
+// import ImagePreview from "./components/test/viewImg";
+
+// import  { useDispatch, useSelector } from "react-redux"
+// import { RootState } from "./redux/store/store";
+// import React from "react";
+// import { showDialog } from "./redux/state/dialogSlice";
+
+// function App() {
+// 	// const dispatch = useDispatch();
+// 	// const open = useSelector((state: RootState) => state.dialogListener.isDialogVisible)
+
+// 	// // logic to determine if there are unsaved changes
+// 	// const hasUnsavedChanges = useSelector((state: RootState) => state.pageAbandonmentPrevention.isDirty)
+
+// 	// // Add event listeners when the component mounts
+// 	// React.useEffect(() => {
+// 	// 	const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+// 	// 		// Check if there are unsaved changes and show a confirmation dialog if needed
+// 	// 		if (hasUnsavedChanges) {
+// 	// 			e.preventDefault();
+// 	// 			e.returnValue = 'Dirty it is'; // This message will be displayed in the confirmation dialog
+// 	// 		}
+// 	// 	};
+
+// 	// 	// const handleLinkClick = (e: MouseEvent) => {
+// 	// 	// 	// Check if there are unsaved changes and show a confirmation dialog if needed
+// 	// 	// 	if (hasUnsavedChanges) {
+// 	// 	// 		e.preventDefault();
+// 	// 	// 		dispatch(showDialog()); // Dispatch your Redux action to show the dialog
+// 	// 	// 	}
+// 	// 	// };
+
+// 	// 	// Add event listeners
+// 	// 	window.addEventListener('beforeunload', handleBeforeUnload, { capture: true });
+// 	// 	// document.addEventListener('click', handleLinkClick);
+
+// 	// 	// Remove event listeners when the component unmounts
+// 	// 	return () => {
+// 	// 		window.removeEventListener('beforeunload', handleBeforeUnload, { capture: true });
+// 	// 		// document.removeEventListener('click', handleLinkClick);
+// 	// 	};
+// 	// }, [dispatch, hasUnsavedChanges]);
+
+// 	return (
+// 		<BrowserRouter>
+// 			<Routes>
+// 				<Route path="/" Component={LoginPage} />
+// 				<Route path="/dashboard" Component={DashboardHandler} />
+// 				<Route path="/users/*" Component={Userhandler} />
+// 				<Route path="/user/add-new-user" Component={CreateUserHandler} />
+// 				{/* <Route path="/edituser" Component={EditUserHandler} /> */}
+// 				<Route path="/user/edit-user/:name/*" element={<EditUserHandler />} />
+// 				<Route path="/project/edit-project/:proj_name" element={<EditProjectHandler />} />
+// 				<Route path="/projects" Component={ProjectHandler} />
+// 				{/* <Route path="/modal" Component={Modal} /> */}
+// 				<Route path="/roles" Component={RoleHandler} />
+// 				<Route path="/project/add-new-project" Component={NewProjHandler} />
+// 				{/* <Route path="/User/:name" Component={UserDetails} /> */}
+// 				<Route path="/development-phase" Component={DevelopmentPhaseHandler} />
+// 				<Route path="/clients" Component={ClientHandler} />
+// 				<Route path="/project-status" Component={ProjectStatusHandler} />
+// 				<Route path="/dashboard-table" Component={ActivityLogDashboardTable} />
+// 				<Route path="/employee-status" Component={EmployeeStatusHandler} />
+// 				<Route path="/employee-position" Component={EmployeePositionHandler} />
+// 				<Route path="/business-unit" Component={BusinessUnitHandler} />
+// 				<Route path="/department" Component={DepartmentHandler} />
+// 				<Route path="/technology" Component={TechnologyHandler} />
+// 				{/* <Route path="/test2" Component={TestHandler} /> */}
+// 				{/* <Route path="/sideNav" Component={Sidenav} /> */}
+// 				{/* <Route path="/topNav" Component={Topnav} /> */}
+// 				{/* <Route path="/NewNav" Component={NewNav} /> */}
+// 				<Route path="/navigations" Component={Navigations} />
+// 				{/* <Route path="/user-graph" Component={UserGraph} /> */}
+// 				<Route path="/development-type" Component={DevelopmentTypeHandler} />
+// 				<Route path="/TestHandler" Component={TestHandler} />
+// 				<Route path="/viewImg" Component={ImagePreview} />
+// 			</Routes>
+// 		</BrowserRouter>
+// 	);
+// }
+
+// export default App;
+
+
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
@@ -5,10 +119,8 @@ import Userhandler from "./pages/Userhandler";
 import CreateUserHandler from "./pages/CreateUserHandler";
 import ProjectHandler from "./pages/ProjectHandler";
 import RoleHandler from "./pages/RoleHandler";
-// import NewProj from "./components/project/new_project/NewProj";
 import NewProjHandler from "./pages/NewProjectHandler";
 import AddMemberTable from "./components/project/AddMemberTable";
-// import UserDetails from "./components/user/edit/EditUser";
 import EditUserHandler from "./pages/EditUserHandler";
 import EditProjectHandler from "./pages/EditProjectHandler"
 import DevelopmentPhaseHandler from "./pages/DevelopmentPhaseHandler";
@@ -29,84 +141,96 @@ import DevelopmentTypeHandler from "./pages/DevelopmentTypeHandler";
 import TestHandler from "./pages/TestHandler";
 import ClientHandler from "./pages/ClientHandler";
 import ImagePreview from "./components/test/viewImg";
-
 import  { useDispatch, useSelector } from "react-redux"
 import { RootState } from "./redux/store/store";
 import React from "react";
 import { showDialog } from "./redux/state/dialogSlice";
 
+// ... (other imports and component definitions)
+
 function App() {
-	// const dispatch = useDispatch();
-	// const open = useSelector((state: RootState) => state.dialogListener.isDialogVisible)
-
-	// // logic to determine if there are unsaved changes
-	// const hasUnsavedChanges = useSelector((state: RootState) => state.pageAbandonmentPrevention.isDirty)
-
-	// // Add event listeners when the component mounts
-	// React.useEffect(() => {
-	// 	const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-	// 		// Check if there are unsaved changes and show a confirmation dialog if needed
-	// 		if (hasUnsavedChanges) {
-	// 			e.preventDefault();
-	// 			e.returnValue = 'Dirty it is'; // This message will be displayed in the confirmation dialog
-	// 		}
-	// 	};
-
-	// 	// const handleLinkClick = (e: MouseEvent) => {
-	// 	// 	// Check if there are unsaved changes and show a confirmation dialog if needed
-	// 	// 	if (hasUnsavedChanges) {
-	// 	// 		e.preventDefault();
-	// 	// 		dispatch(showDialog()); // Dispatch your Redux action to show the dialog
-	// 	// 	}
-	// 	// };
-
-	// 	// Add event listeners
-	// 	window.addEventListener('beforeunload', handleBeforeUnload, { capture: true });
-	// 	// document.addEventListener('click', handleLinkClick);
-
-	// 	// Remove event listeners when the component unmounts
-	// 	return () => {
-	// 		window.removeEventListener('beforeunload', handleBeforeUnload, { capture: true });
-	// 		// document.removeEventListener('click', handleLinkClick);
-	// 	};
-	// }, [dispatch, hasUnsavedChanges]);
-
+	const dummyUsers = [
+	  {
+		id: 1,
+		username: "admin",
+		role: "System Administrator",
+	  },
+	  {
+		id: 2,
+		username: "acct_manager",
+		role: "Account Manager",
+	  },
+	  {
+		id: 3,
+		username: "proj_manager",
+		role: "Project Manager",
+	  },
+	  {
+		id: 4,
+		username: "normal_user",
+		role: "Normal User",
+	  },
+	];
+  
+	const defaultUserId = 3;
+  
+	const currentUser = dummyUsers.find((user) => user.id === defaultUserId);
+  
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" Component={LoginPage} />
-				<Route path="/dashboard" Component={DashboardHandler} />
-				<Route path="/users/*" Component={Userhandler} />
-				<Route path="/user/add-new-user" Component={CreateUserHandler} />
-				{/* <Route path="/edituser" Component={EditUserHandler} /> */}
-				<Route path="/user/edit-user/:name/*" element={<EditUserHandler />} />
-				<Route path="/project/edit-project/:proj_name" element={<EditProjectHandler />} />
-				<Route path="/projects" Component={ProjectHandler} />
-				{/* <Route path="/modal" Component={Modal} /> */}
-				<Route path="/roles" Component={RoleHandler} />
-				<Route path="/project/add-new-project" Component={NewProjHandler} />
-				{/* <Route path="/User/:name" Component={UserDetails} /> */}
-				<Route path="/development-phase" Component={DevelopmentPhaseHandler} />
-				<Route path="/clients" Component={ClientHandler} />
-				<Route path="/project-status" Component={ProjectStatusHandler} />
-				<Route path="/dashboard-table" Component={ActivityLogDashboardTable} />
-				<Route path="/employee-status" Component={EmployeeStatusHandler} />
-				<Route path="/employee-position" Component={EmployeePositionHandler} />
-				<Route path="/business-unit" Component={BusinessUnitHandler} />
-				<Route path="/department" Component={DepartmentHandler} />
-				<Route path="/technology" Component={TechnologyHandler} />
-				{/* <Route path="/test2" Component={TestHandler} /> */}
-				{/* <Route path="/sideNav" Component={Sidenav} /> */}
-				{/* <Route path="/topNav" Component={Topnav} /> */}
-				{/* <Route path="/NewNav" Component={NewNav} /> */}
-				<Route path="/navigations" Component={Navigations} />
-				{/* <Route path="/user-graph" Component={UserGraph} /> */}
-				<Route path="/development-type" Component={DevelopmentTypeHandler} />
-				<Route path="/TestHandler" Component={TestHandler} />
-				<Route path="/viewImg" Component={ImagePreview} />
-			</Routes>
-		</BrowserRouter>
+	  <BrowserRouter>
+		<Routes>
+		  {/* Allow access based on user role */}
+		  {currentUser && currentUser.role === "System Administrator" && (
+			<>
+			 	<Route path="/" Component={LoginPage} />
+ 				<Route path="/dashboard" Component={DashboardHandler} />
+ 				<Route path="/users/*" Component={Userhandler} />
+ 				<Route path="/user/add-new-user" Component={CreateUserHandler} />
+ 				<Route path="/user/edit-user/:name/*" element={<EditUserHandler />} />
+ 				<Route path="/project/edit-project/:proj_name" element={<EditProjectHandler />} />
+ 				<Route path="/projects" Component={ProjectHandler} />
+ 				<Route path="/roles" Component={RoleHandler} />
+ 				<Route path="/project/add-new-project" Component={NewProjHandler} />
+ 				<Route path="/development-phase" Component={DevelopmentPhaseHandler} />
+ 				<Route path="/clients" Component={ClientHandler} />
+ 				<Route path="/project-status" Component={ProjectStatusHandler} />
+ 				<Route path="/dashboard-table" Component={ActivityLogDashboardTable} />
+ 				<Route path="/employee-status" Component={EmployeeStatusHandler} />
+ 				<Route path="/employee-position" Component={EmployeePositionHandler} />
+ 				<Route path="/business-unit" Component={BusinessUnitHandler} />
+ 				<Route path="/department" Component={DepartmentHandler} />
+ 				<Route path="/technology" Component={TechnologyHandler} />
+ 				<Route path="/navigations" Component={Navigations} />
+ 				<Route path="/development-type" Component={DevelopmentTypeHandler} />
+ 				<Route path="/TestHandler" Component={TestHandler} />
+ 				<Route path="/viewImg" Component={ImagePreview} />			
+			</>
+		  )}
+		  {currentUser && currentUser.role === "Account Manager" && (
+			<>
+			  <Route path="/" Component={LoginPage} />
+			  <Route path="/dashboard" Component={DashboardHandler} />
+			  <Route path="/users/*" Component={Userhandler} />
+			  <Route path="/user/add-new-user" element={<CreateUserHandler />} />
+			  <Route path="/user/edit-user/:name/*" element={<EditUserHandler />} />
+			</>
+		  )}
+		  {currentUser && currentUser.role === "Project Manager" && (
+			<>
+			  <Route path="/" Component={LoginPage} />
+ 			  <Route path="/dashboard" Component={DashboardHandler} />
+			  <Route path="/projects" Component={ProjectHandler} />
+			  <Route path="/project/add-new-project" element={<NewProjHandler />} />
+			  <Route path="/project/edit-project/:proj_name" element={<EditProjectHandler />} />
+			</>
+		  )}
+		  {!currentUser && (
+			<p>Normal User - Restrict access to all pages</p>
+		  )}
+		</Routes>
+	  </BrowserRouter>
 	);
-}
-
-export default App;
+  }
+  
+  export default App;
+  
