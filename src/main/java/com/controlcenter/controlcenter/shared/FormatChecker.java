216274cde4ae8574f71f79bd4b87a0a6bf566140
Ricky Galpo <rickygalpo7777@gmail.com>
2023-10-10 09:08:33 +0800

    Added email validation and edit project with validation for changes.

--- src/main/java/com/controlcenter/controlcenter/shared/FormatChecker.java
+++ src/main/java/com/controlcenter/controlcenter/shared/FormatChecker.java
@@ -1,5 +1,8 @@
 package com.controlcenter.controlcenter.shared;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,5 +23,19 @@ public class FormatChecker {
             }
         }
         return false;
+    }
+
+    public boolean isValidEmail(String email) {
+        // Define a regular expression for a basic email pattern.
+        String regex = "^[A-Za-z0-9+_.-]+@([A-Za-z0-9.-]+)$";
+        
+        // Create a Pattern object and compile the regex pattern.
+        Pattern pattern = Pattern.compile(regex);
+        
+        // Create a Matcher object and match the input email against the pattern.
+        Matcher matcher = pattern.matcher(email);
+        
+        // Return true if the email matches the pattern, false otherwise.
+        return matcher.matches();
     }
 }
